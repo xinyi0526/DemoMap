1cf8cee2e91dcf063ec460e508e3bab768f69ab2
xinyi0526 <xinyi.luvand620@gmail.com>
2020-07-09 10:39:28 +0800

    submission

--- app/src/main/java/com/myapplicationdev/android/demomap/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demomap/MainActivity.java
@@ -1,14 +1,123 @@
 package com.myapplicationdev.android.demomap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
+import androidx.fragment.app.FragmentManager;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2,btn3;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        SupportMapFragment mapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+
+                LatLng poi_CausewayPoint = new LatLng(1.436065, 103.786263);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_CausewayPoint,
+                        15));
+
+                map.addMarker(new
+                        MarkerOptions()
+                        .position(poi_CausewayPoint)
+                        .title("Causeway Point")
+                        .snippet("Shopping after class")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+
+                LatLng poi_RP = new LatLng(1.44224, 103.785733);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_RP,
+                        15));
+
+                map.addMarker(new
+                        MarkerOptions()
+                        .position(poi_RP)
+                        .title("Republic Polytechnic")
+                        .snippet("C347 Android Programming II")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
+
+
+
+                UiSettings ui = map.getUiSettings();
+
+                ui.setCompassEnabled(true);
+
+
+                UiSettings ui1 = map.getUiSettings();
+
+                ui1.setZoomControlsEnabled(true);
+
+                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+                if (permissionCheck == PermissionChecker.PERMISSION_GRANTED) {
+                    map.setMyLocationEnabled(true);
+                } else {
+                    Log.e("GMap - Permission", "GPS access has not been granted");
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+                }
+
+
+
+            }
+        });
+
+        btn1 = findViewById(R.id.normal);
+        btn2 = findViewById(R.id.terrain);
+        btn3 = findViewById(R.id.satelite);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(map != null){
+                    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                }
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null){
+                    map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                }
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null){
+                    map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                }
+            }
+        });
+
     }
 }
